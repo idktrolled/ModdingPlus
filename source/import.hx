94be54016d401833ab4d1122701fb67cf40e7f96
idktrolled <camilocarlos1100@gmail.com>
2025-12-17 18:17:26 -0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,12 +1,13 @@
 #if !macro
-#if mobile
-import extension.androidtools.content.Context;
-import extension.androidtools.widget.Toast;
-import extension.androidtools.os.Environment;
-import extension.androidtools.Permissions;
-import extension.androidtools.Settings;
-import extension.androidtools.Tools;
-import extension.androidtools.os.Build.VERSION;
-import extension.androidtools.os.Build.VERSION_CODES;
-#end
+#if android
+ import android.content.Context as AndroidContext;
+ import android.widget.Toast as AndroidToast;
+ import android.os.Environment as AndroidEnvironment;
+ import android.Permissions as AndroidPermissions;
+ import android.Settings as AndroidSettings;
+ import android.Tools as AndroidTools;
+ import android.os.Build.VERSION as AndroidVersion;
+ import android.os.Build.VERSION_CODES as AndroidVersionCode;
+ import android.os.BatteryManager as AndroidBatteryManager;
+ #end
 #end
