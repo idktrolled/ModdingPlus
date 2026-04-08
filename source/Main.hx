705f67ac397dd4bf4417bb09e9513ce379ca1691
idktrolled <camilocarlos1100@gmail.com>
2026-04-08 15:37:18 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -7,9 +7,6 @@ import openfl.display.Sprite;
 import plugins.ExamplePlugin;
 import plugins.ExamplePlugin.ExampleCharPlugin;
 #end
-#if mobile
-import mobile.backend.SUtil;
-#end
 class Main extends Sprite
 {
 	#if sys
@@ -17,19 +14,23 @@ class Main extends Sprite
 	#end
 	public function new()
 	{
+		#if mobile
+		#if android
+		StorageUtil.requestPermissions();
+		#end
+		Sys.setCwd(StorageUtil.getStorageDirectory());
+		#end
 		#if typebuild
 			// god is dead
 			ExamplePlugin;
 			ExampleCharPlugin;
 		#end
 		super();
-		#if mobile
-		SUtil.requestPermissions();
-		#elseif sys
+		#if sys
 		cwd = Sys.getCwd();
 		#end
-		addChild(new FlxGame(0, 0, TitleState, 1, OptionsHandler.options.fpsCap, OptionsHandler.options.fpsCap, true));
-		#if mobile
+		addChild(new FlxGame(0, 0, #if mobile CopyState.checkExistingFiles() ? TitleState : CopyState #else TitleState #end, 1, OptionsHandler.options.fpsCap, OptionsHandler.options.fpsCap, true));
+		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
 		addChild(new MemoryCounter(10, 3, 0xFFFFFF));
 		#end
